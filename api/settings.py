4406d7cb7536ffc64d5a46bd43ed6f365f9d4940
MythicalCosmic <166993229+MythicalCosmic@users.noreply.github.com>
2024-06-30 13:42:14 +0500

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -81,15 +81,10 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pharm',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
